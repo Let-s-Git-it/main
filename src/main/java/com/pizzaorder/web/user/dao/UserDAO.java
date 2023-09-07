4fc3e208403d41124879ae2bc6c317885271e453
yumyumsom <woosw13@gmail.com>
2023-09-07 17:35:49 +0900

    [ver 1.1] userDAO 수정

--- src/main/java/com/pizzaorder/web/user/dao/UserDAO.java
+++ src/main/java/com/pizzaorder/web/user/dao/UserDAO.java
@@ -1,6 +1,5 @@
 package com.pizzaorder.web.user.dao;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,57 +15,57 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import com.pizzaorder.web.mybatis.SqlMapConfig;
 
 public class UserDAO {
-	
-	
-	SqlSessionFactory factory = SqlMapConfig.getFactory();
-	SqlSession sqlSession;
-	
-	public UserDAO() {
-		sqlSession = factory.openSession(true);
-	}
-	
-	//회원가입
-	public boolean join(UserDTO user) {
-		boolean result = false;
-		if(sqlSession.insert("User.join",user) != 0){
-			result = true;
-		}
-		
-		System.out.println(result);
-		return result;
-	}
 
-	public boolean login(String userid, String userpw) {
-		boolean result = false;
-		
-		HashMap<String, String>datas = new HashMap<>();
-		datas.put("userid", userid);
-		datas.put("userpw", userpw);
-		
-		if((Integer)sqlSession.selectOne("User.login",datas) ==1) {
-			result = true;
-		}
-		
-		return result;
-		
-	}
-	
-	
-	// idcheck
-	public boolean idcheck(String userid) {
-	    boolean result = false;
-	    
-	    // 이미 존재하는 사용자인지 확인
-	    int count = sqlSession.selectOne("User.idcheck", userid);
-	    
-	    if (count == 1) {
-	        result = true; // 이미 사용 중인 아이디
-	    }
-	    
-	    return result;
-	}
-	
-	//logout
+
+    SqlSessionFactory factory = SqlMapConfig.getFactory();
+    SqlSession sqlSession;
+
+    public UserDAO() {
+        sqlSession = factory.openSession(true);
+    }
+
+    //회원가입
+    public boolean join(UserDTO user) {
+        boolean result = false;
+        if (sqlSession.insert("User.join", user) != 0) {
+            result = true;
+        }
+
+        System.out.println(result);
+        return result;
+    }
+
+    public boolean login(String userid, String userpw) {
+        boolean result = false;
+
+        HashMap<String, String> datas = new HashMap<>();
+        datas.put("userid", userid);
+        datas.put("userpw", userpw);
+
+        if ((Integer) sqlSession.selectOne("User.login", datas) == 1) {
+            result = true;
+        }
+
+        return result;
+
+    }
+
+
+    // idcheck
+    public boolean checkId(String userid) {
+        boolean result = false;
+
+        // 이미 존재하는 사용자인지 확인
+        int count = sqlSession.selectOne("User.checkId", userid);
+
+        if (count == 1) {
+            result = true; // 이미 사용 중인 아이디
+        }
+
+        return result;
+    }
+
+    //logout
 //	public boolean logout(String userid) {
 //		boolean result = false;
 //		int rowsAffected = sqlSession.delete("User.logout", userid);
@@ -77,6 +76,4 @@ public class UserDAO {
 //		return result;
 //	}
 //	
-
-
 }
