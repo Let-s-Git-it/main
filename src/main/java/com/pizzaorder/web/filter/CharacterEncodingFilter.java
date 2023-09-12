acb165469a66dfd4afe4b7c5b63b7d739c6817e3
yumyumsom <woosw13@gmail.com>
2023-09-12 09:30:26 +0900

    [ver 1.4] 필터 경로 수정

--- src/main/java/com/pizzaorder/web/filter/CharacterEncodingFilter.java
+++ src/main/java/com/pizzaorder/web/filter/CharacterEncodingFilter.java
@@ -14,14 +14,14 @@ public class CharacterEncodingFilter implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest arg0,
-			ServletResponse arg1, FilterChain arg2)
+						 ServletResponse arg1, FilterChain arg2)
 			throws IOException, ServletException {
-	
+
 		//System.out.println("before filter");
 		arg0.setCharacterEncoding("UTF-8");
 
-	 	arg1.setCharacterEncoding("UTF-8");
-	 	//arg1.setContentType("text/html;charset=UTF-8");
+		arg1.setCharacterEncoding("UTF-8");
+		//arg1.setContentType("text/html;charset=UTF-8");
 		arg2.doFilter(arg0, arg1);
 		//System.out.println("after filter");
 	}
--- src/main/java/com/pizzaorder/web/filter/CharacterEncodingUserFilter.java
+++ src/main/java/com/pizzaorder/web/filter/CharacterEncodingUserFilter.java
@@ -14,12 +14,12 @@ public class CharacterEncodingUserFilter implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest arg0,
-			ServletResponse arg1, FilterChain arg2)
+						 ServletResponse arg1, FilterChain arg2)
 			throws IOException, ServletException {
-	
+
 		arg0.setCharacterEncoding("UTF-8");
-	 	arg1.setCharacterEncoding("UTF-8");
-	 	arg1.setContentType("text/html; charset = UTF-8");
+		arg1.setCharacterEncoding("UTF-8");
+		arg1.setContentType("text/html; charset = UTF-8");
 		arg2.doFilter(arg0, arg1);
 	}
 
