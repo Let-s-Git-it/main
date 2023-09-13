7d02f5f252ea70de776144df16fcef5f3a4bb99b
geonwoo roh <gwooroh@naver.com>
2023-09-14 08:53:38 +0900

    logout ver 1.0

---
+++ src/main/java/com/pizzaorder/web/user/Logoutok.java
@@ -0,0 +1,30 @@
+package com.pizzaorder.web.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.pizzaorder.web.user.action.Action;
+import com.pizzaorder.web.user.action.ActionForward;
+import com.pizzaorder.web.user.dao.UserDAO;
+
+
+
+public class Logoutok implements Action {
+    @Override
+    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
+
+        ActionForward forward = new ActionForward();
+
+        // 세션에서 사용자 정보 삭제
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.removeAttribute("userid");
+            session.invalidate(); // 세션 무효화
+        }
+
+        // 로그아웃 후 로그인 페이지로 이동
+        forward.setPath("/login/loginview.jsp");
+        return forward;
+    }
+}
