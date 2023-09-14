661674d3427c9a532c03963995c4b02d2f87dd0c
yumyumsom <woosw13@gmail.com>
2023-09-14 18:38:12 +0900

    [ver 1.4] login 주석 수정

--- src/main/java/com/pizzaorder/web/user/UserLoginOk.java
+++ src/main/java/com/pizzaorder/web/user/UserLoginOk.java
@@ -8,31 +8,6 @@ import com.pizzaorder.web.user.action.Action;
 import com.pizzaorder.web.user.action.ActionForward;
 import com.pizzaorder.web.user.dao.UserDAO;
 
-/*
-public class UserLoginOk implements Action {
-    @Override
-    public ActionForward execute(HttpServletRequest request, HttpServletResponse response) {
-
-        ActionForward forward = new ActionForward();
-        UserDAO udao = new UserDAO();
-        String userid = request.getParameter("userid");
-        String userpw = request.getParameter("userpw");
-
-        if (udao.logincheck(userid, userpw)) {
-            forward.setPath("/base/index.jsp");
-        } else {
-            request.setAttribute("loginError", "아이디 또는 비밀번호가 올바르지 않습니다.");
-            forward.setPath("/login/loginview.jsp");
-        }
-
-//        System.out.println("forward >>>>>>>>>>>>>>>>>>>>>>>>> " + forward.toString());
-
-        return forward;
-
-
-    }
-}
-*/
 
 public class UserLoginOk implements Action {
     @Override
